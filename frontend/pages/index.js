a337540cb7b14a1dc5323d529451cfaef52c1d61
qvil <qvil1127@gmail.com>
2018-09-05 01:25:29 +0900

    Add Write 버튼 및 Link, .gitignore

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Welcome to next.js!</div>;
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/write">
+      <button>게시물 작성</button>
+    </Link>
+  </div>
+);
+
+export default Index;
