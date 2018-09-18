444e21d1ea10890b24cc0b27cbeae504b1b1e221
Taesu Hyeon <qvil1127@gmail.com>
2018-09-19 00:13:20 +0900

    Write 추가
    * import React 안해도 Class Component 가능

--- frontend/pages/write.js
+++ frontend/pages/write.js
@@ -1,7 +1,39 @@
-import React from "react";
+// import React from "react";
+import Button from "../components/Button";
 
-const Write = () => {
-  return <div>Write</div>;
-};
+class Write extends React.Component {
+  state = {
+    title: "",
+    content: "",
+    author: ""
+  };
 
+  handleSubmit = () => {
+    console.log("submit");
+
+    // fetch(
+    //   `http://mugle.org/PilotBoard/create?id=${"id"}&content=${"content"}&title=${"title"}`
+    // );
+  };
+
+  render() {
+    const { handleSubmit } = this;
+    const { title, content, author } = this.state;
+
+    return (
+      <div>
+        <div>
+          title :<input value={title} />
+        </div>
+        <div>
+          content :<input value={content} />
+        </div>
+        <div>
+          작성자 :<input value={author} />
+        </div>
+        <Button onClick={handleSubmit}>submit</Button>
+      </div>
+    );
+  }
+}
 export default Write;
